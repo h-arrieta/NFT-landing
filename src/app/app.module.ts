9b427fd94c22bb6154ed43b18e6da56c3bac6c57
hector <hector.arrieta@techedgegroup.com>
2022-05-12 23:51:50 +0200

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AppbarComponent } from './appbar/appbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    colors: {
+      // corporative: 'transparent',
+      blue: '#374151',
+      corp: '#FF833E',
+      subtext: '#8A939B',
+      text: '#353840',
+      label: '#04111D',
+      background: '#FBFDFF',
+      
+    },
+    height: {
+      nft: '500px',
+     },
+    // width: {
+    //   nft: '600px',
+    //  },
+  },
+  plugins: [
+],
+}
